1552f6d6e37fd6851fca37aac372b5b242263274
Jan Iwanow <iwanow.jan@gmail.com>
2021-11-29 22:59:49 +0700

    вынес подключаемые файлы в отдельный файл

---
+++ includes.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+// Подключаем необходимые файлы
+//
+// require_once используется во избежание
+// многократного подключения одного и того же файла
+$files = [
+    __DIR__ . '/framework/functions.php',
+    __DIR__ . '/framework/Route.php',
+    __DIR__ . '/framework/Router.php',
+    __DIR__ . '/framework/Session.php',
+    __DIR__ . '/framework/Authenticator.php',
+];
+
+foreach ($files as $file) {
+    require_once $file;
+}
