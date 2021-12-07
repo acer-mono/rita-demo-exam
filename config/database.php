90788e51501c28e7037cf997207df02565e7795e
Jan Iwanow <iwanow.jan@gmail.com>
2021-12-07 18:58:15 +0700

    поправил .gitignore

--- config/database.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types=1);
-
-return [
-    'DB_DSN' => 'mysql:host=127.0.0.1;port=3366;dbname=rita_exam;charset=utf8mb4',
-    'DB_USER' => 'rita_exam',
-    'DB_PASS' => 'rita_exam'
-];
